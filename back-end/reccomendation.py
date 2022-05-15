08c2e6ecd420a17fd708f159ce0b8f6ed4aba90e
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-05-14 19:22:03 -0600

    recomendation algorythm start

--- back-end/reccomendation.py
+++ back-end/reccomendation.py
@@ -1,1 +1,15 @@
 print("omg hi")
+libreria = [
+    {
+        "name": "La sombra del viento", 
+        "isbn": "372908173890",
+        "edicion": "4"
+    },
+    {
+        "name": "El juego del angel", 
+        "isbn": "372908173891",
+        "edicion": "2"
+    }
+]
+for item in  libreria:
+    print(item)
